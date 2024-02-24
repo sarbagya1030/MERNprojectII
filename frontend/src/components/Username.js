f23210780d076d1237fcde87c050727d8a2c25a1
Sarbagya Sapkota <sarbagya.sapkota@gmail.com>
2024-02-24 20:22:32 +0545

    firstday

--- frontend/src/components/Username.js
+++ frontend/src/components/Username.js
@@ -1,14 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.jpg";
+import styles from "../styles/Username.module.css";
 
 export default function Username() {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-center items-center h-screen">
-        <div>
+      <div className="flex h-screen">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold">Hello again</h4>
-            <span className="py-4 text-xl w-2/3 text-center"></span>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore more
+            </span>
           </div>
+
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img
+                src={avatar}
+                className={styles.profile_img}
+                alt="avatar"
+              ></img>
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input type="text" placeholder="Username"></input>
+              <button type="submit">Let's Go</button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Not a Member
+                <Link className="text-red-500" to="/register">
+                  Register Now
+                </Link>
+              </span>
+            </div>
+          </form>
         </div>
       </div>
     </div>
