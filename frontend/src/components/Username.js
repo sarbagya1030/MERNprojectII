0dcce44e2a94b67226e6bf18eceab03d45d48129
Sarbagya Sapkota <sarbagya.sapkota@gmail.com>
2024-02-24 17:22:51 +0545

    new

---
+++ frontend/src/components/Username.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Username() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello again</h4>
+            <span className="py-4 text-xl w-2/3 text-center"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
