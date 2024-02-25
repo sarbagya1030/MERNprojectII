513d88bed0dd143d44192ab7c6a5f4ff9043f95f
Sarbagya Sapkota <sarbagya.sapkota@gmail.com>
2024-02-25 19:01:07 +0545

    registerUIcomplete

--- frontend/src/helper/validate.js
+++ frontend/src/helper/validate.js
@@ -22,6 +22,15 @@ export async function resetPasswordValidation(values) {
   return errors;
 }
 
+//validate register form
+export async function registerValidation(values) {
+  const errors = usernameVerify({}, values);
+  passwordVerify(errors, values);
+  emailVerify(errors, values);
+
+  return errors;
+}
+
 //.........................................................
 
 //validate password
@@ -51,6 +60,19 @@ function usernameVerify(error = {}, values) {
   //   else if (values.username.includes("")) {
   //     error.username = toast.error("Invalid Username...!");
   //   }
+
+  return error;
+}
+
+//validate email
+function emailVerify(error = {}, values) {
+  if (!values.email) {
+    error.email = toast.error("Email Required...!");
+  } else if (/\s/.test(values.email)) {
+    error.email = toast.error("Wrong Email...!");
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    error.email = toast.error("Invalid email address...!");
+  }
 
   return error;
 }
