3b770186abf15af5e447d599cbb9fce073cca433
Sarbagya Sapkota <sarbagya.sapkota@gmail.com>
2024-03-18 16:21:01 +0545

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { Console } from "console";
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
+
+const port = 8080;
+
+//http get request
+app.get("/", (req, res) => {
+  res.status(201).json("Home GET Request");
+});
+
+//start server
+app.listen(port, () => {
+  console.log(`Server connected to http://localhost:${port}`);
+});
